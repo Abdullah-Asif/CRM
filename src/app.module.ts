0bca0389947db7161981324a4505a3f644618cdb
Asif <asifabdullah135@gmail.com>
2024-02-27 22:09:11 +0600

    app module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import {CustomerController} from "./wepApi/controllers/customer/customer.controller";
+import {InfrastructureModule} from "./infrastructure/infrastructure.module";
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [InfrastructureModule],
+  controllers: [CustomerController],
+  providers: []
 })
 export class AppModule {}
