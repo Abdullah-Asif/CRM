c5fec907ca6fa01de85ed5ccfd1c751a009477e3
Asif <asifabdullah135@gmail.com>
2024-02-28 21:16:59 +0600

    return demo data using faker

--- src/wepApi/controllers/customer/customer.controller.ts
+++ src/wepApi/controllers/customer/customer.controller.ts
@@ -2,7 +2,7 @@ import {Body, Controller, Get, Inject, Post} from '@nestjs/common';
 import {Customer} from "../../../domain/entities/customer.entity";
 import {CreateCustomerRequest} from "./requestModels/createCustomerRequest";
 import {ICustomerService} from "../../../application/contracts/services/customer.service.interface";
-
+import {faker} from '@faker-js/faker'
 @Controller('customers')
 export class CustomerController {
     constructor(private  readonly _customerService: ICustomerService) {
@@ -10,7 +10,7 @@ export class CustomerController {
     @Get()
     public async getAll(): Promise<Customer[]> {
         //mapper will use in this part. return customerDto
-        return await this._customerService.getAllCustomer();
+        return customers;
     }
 
     @Post()
@@ -18,3 +18,21 @@ export class CustomerController {
         // await this._customerService.insert()
     }
 }
+
+
+
+
+
+
+
+export function createRandomCustomer() : Customer {
+    return {
+        id: faker.number.int({min: 10, max: 100}),
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        organization: faker.company.name(),
+        country: faker.location.country()
+    }
+}
+
+export const customers: Customer[] = faker.helpers.multiple(createRandomCustomer, {count: 100})
