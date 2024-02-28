30018c97d2a814a50debc812aa42d077b5367548
Asif <asifabdullah135@gmail.com>
2024-02-28 21:17:28 +0600

    file renamed

--- src/infrastructure/persistence/repositories/customer.repository.ts
+++ src/infrastructure/persistence/repositories/customer.repository.ts
@@ -1,4 +1,4 @@
-import { MssqlRepository} from "./mssqlRepository";
+import { MssqlRepository} from "./mssql.repository";
 import {Customer} from "../../../domain/entities/customer.entity";
 
 export class CustomerRepository extends MssqlRepository<Customer> {
